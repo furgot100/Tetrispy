1293af236b2f636735f6100d45abdd83df1b193b
Francis Tsang <frtsang40@gmail.com>
2021-07-27 13:14:26 -0700

    coded basic game logic

--- tetris.py
+++ tetris.py
@@ -116,6 +116,11 @@ T = [['.....',
 shapes = [S, Z, I, O, J, L, T]
 shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
 
+top_left_x = (s_width - play_width) // 2
+top_left_y = s_height - play_height
+
+
+
 class Piece(object):
     rows = 20
     columns = 10
@@ -126,3 +131,84 @@ class Piece(object):
         self.shape = shape
         self.color = shape_colors(shapes.index(shape))
         self.rotataion = 0 
+
+# makes gird and locks postion so pieces don't go off grid
+def create_grid(locked_positions={}):
+    grid = [[(0,0,0) for x in range(10)] for x in range(20)] #20 rows and 10 columns
+    
+    for i in range(len(grid)) :
+        for j in range(len(grid[i])):
+            if (j,i) in locked_positions:
+                c = locked_positions[(j,i)]
+                grid[i][j] = c
+    return grid
+
+# returns a random shape
+def get_shape():
+    global shapes, shape_colors
+
+    return Piece(5,0, random.choice(shapes))
+
+def draw_grid(surface, grid):
+    surface.fill((0,0,0)) #Black background
+    # Tetris Title
+    font = pygame.font.SysFont('comicsans', 60)
+    label = font.render('TETRIS', 1, (255,255,255))
+
+    surface.blit(label, (top_left_x + play_width / 2 - (label.get_width() / 2), 30))
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            pygame.draw.rect(surface, grid[i][j], (top_left_x + j* 30, top_left_y + i * 30, 30, 30), 0)
+
+    # draw grid and border
+    draw_grid(surface, 20, 10)
+    pygame.draw.rect(surface, (255, 0, 0), (top_left_x, top_left_y, play_width, play_height), 5)
+    pygame.display.update()
+
+
+def draw_window(surface):
+    pass
+
+
+def main ():
+    global grid
+ 
+    locked_positions = {}  # (x,y):(255,0,0)
+    grid = create_grid(locked_positions)
+ 
+    change_piece = False
+    run = True
+    current_piece = get_shape()
+    next_piece = get_shape()
+    clock = pygame.time.Clock()
+    fall_time = 0
+ 
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.display.quit()
+                quit()
+ 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    current_piece.x -= 1
+ 
+                elif event.key == pygame.K_RIGHT:
+                    current_piece.x += 1
+                elif event.key == pygame.K_UP:
+                    # rotate shape
+                    current_piece.rotation += 1
+ 
+                if event.key == pygame.K_DOWN:
+                    # move shape down
+                    current_piece.y += 1
+
+        draw_window(win)
+
+
+win = pygame.display.set_mode((s_width, s_height))
+pygame.display.set_caption('Tetris')
+
+main()
